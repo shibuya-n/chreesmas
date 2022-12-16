9f37b7c6ebe04b91299a092670e1880c4a2a6166
shibuya-n <a.duong2006@gmail.com>
2022-12-16 10:09:52 -0800

    chrresmasss

--- src/WordSearch.java
+++ src/WordSearch.java
@@ -1,3 +1,7 @@
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class WordSearch {
     public String[][] letters;
 
@@ -6,10 +10,31 @@ public class WordSearch {
     }
 
     public void guess(String word) {
+
+
         System.out.println("Guessed word " + "'" + word + "'" + " not found");
     }
 
     public static void main(String[] args) {
-        WordSearch crossword = new WordSearch(ChristmasWordSearch.letters);
+        ask();
+
+
+    }
+
+    public static void ask() {
+        Scanner wordScanner = new Scanner(System.in);
+        System.out.println("Insert word for word search: ");
+
+        String wordInput = wordScanner.nextLine().trim();
+        Pattern sortNum = Pattern.compile("[A-Za-z]");
+        Matcher matcher = sortNum.matcher(wordInput);
+        boolean inputFound = matcher.find();
+
+        if (inputFound) {
+            System.out.println(wordInput);
+            WordSearch crossword = new WordSearch(ChristmasWordSearch.letters);
+            crossword.guess(wordInput);
+        }
+
     }
 }
