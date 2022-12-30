e02c24418a4e91a742ad573f219856ae130ef524
Andy Duong <a.duong2006@gmail.com>
2022-12-30 15:19:16 -0800

    update

--- src/WordSearch.java
+++ src/WordSearch.java
@@ -9,31 +9,52 @@ public class WordSearch {
         this.letters = letters;
     }
 
-    public void guess(String word) {
-
-
-        System.out.println("Guessed word " + "'" + word + "'" + " not found");
-    }
-
     public static void main(String[] args) {
         ask();
 
 
     }
 
+
+    public void guess(String word) {
+        for (int i = 0; i < letters.length; i++) {
+            for (int j = 0; j < letters[i].length; j++) {
+                for (int k = 0; k < word.length(); k++) {
+                    if (letters[i][j].equals(word.substring(k))) {
+                        System.out.println("Found '" + word.substring(k) + "' At: " + i + ", " + j);
+
+                    }
+
+
+
+                }
+            }
+        }
+        System.out.println("Guessed word " + "'" + word + "'" + " not found");
+        ask();
+
+
+    }
+
+
     public static void ask() {
         Scanner wordScanner = new Scanner(System.in);
         System.out.println("Insert word for word search: ");
 
         String wordInput = wordScanner.nextLine().trim();
-        Pattern sortNum = Pattern.compile("[A-Za-z]");
+        Pattern sortNum = Pattern.compile("^[a-zA-Z]*$");
         Matcher matcher = sortNum.matcher(wordInput);
         boolean inputFound = matcher.find();
 
         if (inputFound) {
-            System.out.println(wordInput);
+
             WordSearch crossword = new WordSearch(ChristmasWordSearch.letters);
-            crossword.guess(wordInput);
+            crossword.guess(wordInput.toUpperCase());
+        }
+
+        else {
+            System.out.println("Invalid Syntax | Please input a word.");
+            ask();
         }
 
     }
